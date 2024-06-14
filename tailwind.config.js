dd5fcc4da396b73eedfd33f43bee5c9987042402
AleesMP <alejandromperis@gmail.com>
2024-06-14 18:11:01 +0200

    testing tailwind in prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    './index.html',
-    './scripts.js',
-    './assets/css/style.css',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
